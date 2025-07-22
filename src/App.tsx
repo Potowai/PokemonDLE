5f5c6935d711d7955aed4a762c84ea480d294b72
Potowai <81572547+Potowai@users.noreply.github.com>
2025-07-22 21:03:31 +0200

    chore: remove unused React import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import React from 'react';
 import { GameHeader } from './components/GameHeader';
 import { PokemonSearch } from './components/PokemonSearch';
 import { ComparisonTable } from './components/ComparisonTable';
